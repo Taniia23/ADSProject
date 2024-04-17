516e12ad47fc7e3ba805ad70d8fdc485e935e7bd
Taniia23 <164653734+Taniia23@users.noreply.github.com>
2024-04-16 23:29:37 -0600

    validaciones de grupo

--- ADSProject-main/ADSProject/ADSProject/Models/Grupo.cs
+++ ADSProject-main/ADSProject/ADSProject/Models/Grupo.cs
@@ -5,14 +5,13 @@ namespace ADSProject.Models
     public class Grupo
     {
         public int IdGrupo { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
         public int IdCarrera { get; set; }
-        
-        public int IdMateria { get; set; }
-        
+        [Required(ErrorMessage = "Este campo es requerido")] public int IdMateria { get; set; }
         public int IdProfesor { get; set; }
-      
+        [Required(ErrorMessage = "Este campo es requerido")]
         public int Ciclo { get; set; }
-       
+        [Required(ErrorMessage = "Este campo es requerido")]
         public int Anio { get; set; }
     }
 }
